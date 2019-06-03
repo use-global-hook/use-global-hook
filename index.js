9a192d1f64109b5ffa283e7cf9b459119186a1cb
Oleksandr Lisovyk <BigLes@users.noreply.github.com>
2019-06-03 13:10:51 +0200

    Improving performance in the loops
    forEach loop is 30% slover than regular for loop, that's why I purpose using regular for loops instead of forEach
    https://jsperf.com/foreach-vs-loop/43

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function setState(newState) {
+  const listenersLength = this.listeners.length;
   this.state = { ...this.state, ...newState };
-  this.listeners.forEach((listener) => {
-    listener(this.state);
-  });
+  for (let i = 0; i < listenersLength; i++) {
+    this.listeners[i](this.state);
+  }
 }
 
 function useCustom(React) {
@@ -18,14 +19,17 @@ function useCustom(React) {
 
 function associateActions(store, actions) {
   const associatedActions = {};
-  Object.keys(actions).forEach((key) => {
+  const actionsKeys = Object.keys(actions);
+  const actionsKeysLength = actionsKeys.length;
+  for (let i = 0; i < actionsKeysLength; i++) {
+    const key = actionsKeys[i];
     if (typeof actions[key] === 'function') {
       associatedActions[key] = actions[key].bind(null, store);
     }
     if (typeof actions[key] === 'object') {
       associatedActions[key] = associateActions(store, actions[key]);
     }
-  });
+  };
   return associatedActions;
 }
 
