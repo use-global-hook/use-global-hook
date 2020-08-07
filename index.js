ee2ece9926347cf937f9a2d9406a8d95a55c9373
André Gardi <andre.gardi@thoughtworks.com>
2020-08-07 18:17:20 -0300

    Only update state if a newState is provided

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function setState(store, newState, afterUpdateCallback) {
-  store.state = Object.assign(store.state, newState);
+  if (newState) store.state = { ...store.state, ...newState };
   store.listeners.forEach((listener) => {
     listener.run(store.state);
   });
