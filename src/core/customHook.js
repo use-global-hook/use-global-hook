dd4bd49b98d92c856a8a9e422da13fdc1d88e3ce
Roman Conch <300konya@gmail.com>
2021-06-03 10:31:22 +0300

    remove React argument and use peerDependencies
    there is no need to pollute arguments as peerDependencies will work much easier
    
    breaking change: remove React from hook arguments

--- src/core/customHook.js
+++ src/core/customHook.js
@@ -1,14 +1,15 @@
+import * as React from 'react';
 import { newListenerEffect } from "./newListenerEffect";
 
-const getMappedActions = (store, React, mapActions) =>
+const getMappedActions = (store, mapActions) =>
   React.useMemo(
     () => (mapActions ? mapActions(store.actions) : store.actions),
     [mapActions, store.actions]
   );
 
-export function customHook(store, React, mapState, mapActions) {
+export function customHook(store, mapState, mapActions) {
   const state = mapState ? mapState(store.state) : store.state;
-  const actions = getMappedActions(store, React, mapActions);
+  const actions = getMappedActions(store, mapActions);
 
   const originalHook = React.useState(Object.create(null))[1];
 
