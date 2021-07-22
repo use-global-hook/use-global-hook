175f64994f5c8aef7236523b7381fde13666335e
Roman Conch <300konya@gmail.com>
2021-07-22 21:39:58 +0300

    refactor: revert removal of afterUpdateCallback

--- src/core/setState.js
+++ src/core/setState.js
@@ -1,4 +1,5 @@
-export function setState(store, newState) {
+export function setState(store, newState, afterUpdateCallback) {
   store.state = { ...store.state, ...newState };
   store.runListeners();
+  afterUpdateCallback && afterUpdateCallback();
 }
