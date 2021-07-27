06b3000d6f87551791344f0fd6549a36d7380be4
Roman Conch <300konya@gmail.com>
2021-07-27 20:35:09 +0300

    feat: add possibility to set compare function

--- src/core/customHook.js
+++ src/core/customHook.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { newListenerEffect } from "./newListenerEffect";
 
-export function customHook(store, mapState, mapActions) {
+export function customHook(store, mapState, mapActions, compare) {
   const state = mapState ? mapState(store.state) : store.state;
   const actions = mapActions ? mapActions(store.actions) : store.actions;
 
   const originalHook = React.useState(state)[1];
+  const resultObject = [state, actions];
 
-  React.useEffect(newListenerEffect(store, state, mapState, originalHook), []); // eslint-disable-line
+  let localCompare;
+  const getComparator = () => localCompare;
 
-  return [state, actions];
+  resultObject.shouldUpdate = (compare) => {
+    localCompare = compare;
+
+    return resultObject;
+  };
+
+  React.useEffect(() => newListenerEffect(store, state, mapState, originalHook, getComparator), []); // eslint-disable-line
+
+  return resultObject;
 }
--- src/core/newListenerEffect.js
+++ src/core/newListenerEffect.js
@@ -1,16 +1,24 @@
 import { cleanUpListener } from "./cleanUpListener";
 
-export const newListenerEffect = (store, oldState, mapState, originalHook) => {
+export const newListenerEffect = (store, oldState, mapState, originalHook, getComparator) => {
   const newListener = { oldState };
-  newListener.run = mapState
-    ? (newState) => {
-        const mappedState = mapState(newState);
-        if (mappedState !== newListener.oldState) {
-          newListener.oldState = mappedState;
-          originalHook(mappedState);
-        }
-      }
-    : originalHook;
+  newListener.run = (newState) => {
+    const compare = getComparator();
+
+    if (!mapState && !compare) {
+      return originalHook(newState);
+    }
+
+    const mappedState = mapState ? mapState(newState) : newState;
+    const updateNeeded = compare
+      ? compare(newListener.oldState, mappedState)
+      : mappedState !== newListener.oldState;
+
+    if (updateNeeded) {
+      newListener.oldState = mappedState;
+      originalHook(newState);
+    }
+  };
 
   store.listeners.push(newListener);
   return cleanUpListener(store, newListener);
